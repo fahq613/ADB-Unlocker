09fb74a826b944f28d7ff7d2af9ee9694365134f
fahq613 <codycryderman@gmail.com>
2026-03-16 09:40:34 -0400

    Update build.gradle.kts with Kotlin version 1.9.23

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 buildscript {
-    ext.kotlin_version = '1.9.23' // or newer, if available
+    ext.kotlin_version = "1.9.23" // or newer, if available
     dependencies {
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23"
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
     }
 }
